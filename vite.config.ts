01b9f1be47ab003463938e974b228ebdedf537eb
baluprakash4 <161422018+baluprakash4@users.noreply.github.com>
2025-05-24 13:12:57 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/', // 👈 your GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
